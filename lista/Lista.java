cf94355eb08b9904aaef57526fc4d50aab5a5cff
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-10-23 16:02:22 +0000

    lista simplemente ligada inconclusa

--- lista/Lista.java
+++ lista/Lista.java
@@ -23,7 +23,96 @@ public class Lista <T>{
         else
            return false;
     }
-    
+
+    /**
+     * 
+     * @param valor
+     */
+   
+    public void agregar(T valor){
+        Nodo<T> nuevo = new Nodo<T>();
+        //fija el valor en el nuevo nodo
+        nuevo.setValor(valor);
+        if(this.esVacia()){
+            cabeza = nuevo;
+        }else{
+            //agregar al final de la lista
+            Nodo<T> aux = cabeza;
+            while (aux.getSiguiente()!=null){
+                aux = aux.getSiguiente();
+            }
+            aux.setSiguiente(nuevo);
+        }
+        tamanio++;
+    }
+
+    public void insertar(T valor, int pos) throws PosicionIlegalException{
+        if(pos>=0 && pos<=tamanio){
+            Nodo<T> nuevo = new Nodo<T>();
+            nuevo.setValor(valor);
+            //el nodo se inserta al principio de la lista
+            if(pos==0){
+                nuevo.setSiguiente(cabeza);
+                cabeza = nuevo;
+
+            }else{
+                //el nodo a insertar va al final de la lista
+                if(pos == tamanio){
+                    Nodo<T> aux = cabeza;
+                    while (aux.getSiguiente()!=null){
+                        aux = aux.getSiguiente();
+                    }
+                    aux.setSiguiente(nuevo);
+
+                }
+                //insertar en medio de la lista
+                else{
+                    Nodo<T> aux = cabeza;
+                    for(int i=0;i<=pos-2;i++){
+                        aux = aux.getSiguiente();
+
+                    }
+                    Nodo<T> siguiente = aux.getSiguiente();
+                    aux.setSiguiente(nuevo);
+                    nuevo.setSiguiente(siguiente);
+
+
+                }
+            }
+
+        }else{
+            throw new PosicionIlegalException();
+        }
+    }
+
+    /**
+     * 
+     * @param pos
+     * @return el valor del elemento que se encuentra en pos si no retorna una excepcion
+     * @throws PosicionIlegalException
+     */
+
+    public T getValor(int pos) throws PosicionIlegalException{
+        if(pos>=0 && pos<tamanio){
+            T valor;
+            if(pos == 0){
+                valor = cabeza.getValor();
+                return valor;
+            }else{
+                Nodo<T> aux = cabeza;
+                for(int i=0;i<=pos-1;i++){
+                    aux = aux.getSiguiente();
+                }
+                valor = aux.getValor();
+            }
+            return valor;
+
+        }else{
+            throw new PosicionIlegalException();
+        }
+
+    }
+
 
     
 
